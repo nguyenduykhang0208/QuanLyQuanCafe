34c65c211a4d620e49b6a1ed8dd1cd3c9771d5a5
Nguyễn Duy Kháng <79954193+nguyenduykhang0208@users.noreply.github.com>
2022-07-16 14:58:15 +0700

    Add files via upload

---
+++ TableManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCoffee
+{
+    public partial class TableManager : Form
+    {
+        public TableManager()
+        {
+            InitializeComponent();
+        }
+
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Login f = new Login();
+            this.Hide();
+            f.ShowDialog();
+            this.Show();
+        }
+
+        private void thôngTinCáNhânToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AccountProfile f = new AccountProfile();
+            f.ShowDialog();     
+        }
+
+        private void adminToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Admin f = new Admin();
+            f.ShowDialog();
+
+        }
+    }
+}
